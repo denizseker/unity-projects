34d59262d345a0c96b696269f61c859fedd4fddc
denizseker <seker.deniz@outlook.com>
2022-08-09 12:55:19 +0300

    commit 2
    added sprites and scripts with some settings

---
+++ Flappy Bird/Assets/Scripts/BirdControl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdControl : MonoBehaviour
+{
+
+    public float velocity = 1f;
+    public Rigidbody2D rb2D;
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
